32b94bc9bd09895b405ad563bd4d764484f682b9
Alek30k <alemank23k@gmail.com>
2023-02-02 13:20:17 -0300

    last commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Add from "./pages/Add";
 import Books from "./pages/Books";
+import Update from "./pages/Update";
 
 function App() {
   return (
@@ -7,8 +9,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Books />} />
-          <Route path="/add" element={<Books />} />
-          <Route path="/update" element={<Books />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/update" element={<Update />} />
         </Routes>
         Hello
       </BrowserRouter>
