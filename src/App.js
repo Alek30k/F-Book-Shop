4f76c534082ce58471b2318e38247b152fd29acc
Alek30k <alemank23k@gmail.com>
2023-02-02 13:13:51 -0300

    last commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>Hello</BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
