285fad708359a140ab1e6e973c7e77c5cbc702a6
Alek30k <alemank23k@gmail.com>
2023-02-03 16:02:40 -0300

    last commit

--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Update = () => {
   const [book, setBook] = useState({
