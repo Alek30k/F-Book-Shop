9562774c0b3ec3c0948fcd23c7c3494ebdb12386
Alek30k <alemank23k@gmail.com>
2023-02-02 21:41:06 -0300

    last commit

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Add = () => {
-  return <div>Add</div>;
+  const [book, setBook] = useState({
+    title: "",
+    desc: "",
+    price: null,
+    cover: "",
+  });
+
+  return (
+    <div className="form">
+      <h1>Add New Book</h1>
+      <input
+        type="text"
+        placeholder="Book title"
+        name="title"
+        // onChange={handleChange}
+      />
+      <textarea
+        rows={5}
+        type="text"
+        placeholder="Book desc"
+        name="desc"
+        // onChange={handleChange}
+      />
+      <input
+        type="number"
+        placeholder="Book price"
+        name="price"
+        // onChange={handleChange}
+      />
+      <input
+        type="text"
+        placeholder="Book cover"
+        name="cover"
+        // onChange={handleChange}
+      />
+    </div>
+  );
 };
 
 export default Add;
