b1622f41a76dfc4b54f5d84f447be59f4ad46bcf
Alek30k <alemank23k@gmail.com>
2023-02-03 10:24:36 -0300

    last commit

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -8,6 +8,10 @@ const Add = () => {
     cover: "",
   });
 
+  const handleChange = (e) => {
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
   return (
     <div className="form">
       <h1>Add New Book</h1>
@@ -15,26 +19,26 @@ const Add = () => {
         type="text"
         placeholder="Book title"
         name="title"
-        // onChange={handleChange}
+        onChange={handleChange}
       />
       <textarea
         rows={5}
         type="text"
         placeholder="Book desc"
         name="desc"
-        // onChange={handleChange}
+        onChange={handleChange}
       />
       <input
         type="number"
         placeholder="Book price"
         name="price"
-        // onChange={handleChange}
+        onChange={handleChange}
       />
       <input
         type="text"
         placeholder="Book cover"
         name="cover"
-        // onChange={handleChange}
+        onChange={handleChange}
       />
     </div>
   );
